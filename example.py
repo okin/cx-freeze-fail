9db4e33176ea2afed6c3504ad443ab609f7bc1c6
Niko Wenselowski <n.wenselowski@uib.de>
2015-06-24 16:55:15 +0200

    Removing unneeded code.

--- example.py
+++ example.py
@@ -27,60 +27,6 @@ def librsyncSignature(filename, base64Encoded=True):
         raise Exception(u"Failed to get librsync signature: %s" % e)
 
 
-def librsyncPatchFile(oldfile, deltafile, newfile):
-    logger.debug(u"Librsync : %s, %s, %s" % (oldfile, deltafile, newfile))
-    if oldfile == newfile:
-        raise ValueError(u"Oldfile and newfile are the same file")
-    if deltafile == newfile:
-        raise ValueError(u"deltafile and newfile are the same file")
-    if deltafile == oldfile:
-        raise ValueError(u"oldfile and deltafile are the same file")
-
-    (of, df, nf, pf) = (None, None, None, None)
-    bufsize = 1024*1024
-    try:
-        of = open(oldfile, "rb")
-        df = open(deltafile, "rb")
-        nf = open(newfile, "wb")
-        pf = librsync.PatchedFile(of, df)
-        data = True
-        while(data):
-            data = pf.read(bufsize)
-            nf.write(data)
-        nf.close()
-        pf.close()
-        df.close()
-        of.close()
-    except Exception as e:
-        if nf: nf.close()
-        if pf: pf.close()
-        if df: df.close()
-        if of: of.close()
-        raise Exception(u"Failed to patch file: %s" % e)
-
-
-def librsyncDeltaFile(filename, signature, deltafile):
-    (f, df, ldf) = (None, None, None)
-    bufsize = 1024*1024
-    try:
-        f = open(filename, "rb")
-        df = open(deltafile, "wb")
-        ldf = librsync.DeltaFile(signature, f)
-
-        data = True
-        while(data):
-            data = ldf.read(bufsize)
-            df.write(data)
-        df.close()
-        f.close()
-        ldf.close()
-    except Exception as e:
-        if df:  df.close()
-        if f:   f.close()
-        if ldf: ldf.close()
-        raise Exception(u"Failed to write delta file: %s" % e)
-
-
 if __name__ == '__main__':
     print("Sig of self: {0}".format(librsyncSignature(__file__)))
 
